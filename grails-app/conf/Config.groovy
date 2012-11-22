0c0d036571720389f6535247764cddc66167e40a
pmbohm <pmbohm@80f082c3-aa3d-4a8a-bc4c-16289f4b763f>
2012-11-22 06:01:37 +0000

    change to dbdev for the moment

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -72,11 +72,15 @@ environments {
 
 // log4j configuration
 log4j = {
+	
+	
     // Example of changing the log pattern for the default console appender:
     //
-    //appenders {
-    //    console name:'stdout', layout:pattern(conversionPattern: '%c{2} %m%n')
-    //}
+    appenders {
+		//    console name:'stdout', layout:pattern(conversionPattern: '%c{2} %m%n')
+		//disable stacktrace file
+		'null' name:'stacktrace'
+    }
 
     error  'org.codehaus.groovy.grails.web.servlet',        // controllers
            'org.codehaus.groovy.grails.web.pages',          // GSP

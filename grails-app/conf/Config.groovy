486cac2872ac5cda36885b281095769d82f4f954
pmbohm <pmbohm@80f082c3-aa3d-4a8a-bc4c-16289f4b763f>
2012-11-25 22:52:00 +0000

    change database to localhost

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -78,6 +78,7 @@ log4j = {
     //
     appenders {
 		//    console name:'stdout', layout:pattern(conversionPattern: '%c{2} %m%n')
+		
 		//disable stacktrace file
 		'null' name:'stacktrace'
     }
